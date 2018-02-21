950b210ddbe1e3adb9849c2accfb33f45e7cba6e
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2018-02-21 16:24:46 -0500

    logo added and menu toggles

--- rmllc_frontend/public/app.js
+++ rmllc_frontend/public/app.js
@@ -1,1 +1,24 @@
 // console.log("app.js is functioning.")
+$(()=>{
+
+  // Starting points for values
+  var menuOpen = false;
+
+  // Toggling the menu bar buttons
+  const menuToggle = () => {
+    if (menuOpen == false) {
+      $('#allButtons').css('display','block');
+      menuOpen = true;
+    } else {
+      $('#allButtons').css('display','none');
+      menuOpen = false;
+    };
+  };
+  $('#topMenu').click(menuToggle);
+  $('#elecButton').click(menuToggle);
+  $('#hvacButton').click(menuToggle);
+  $('#refButton').click(menuToggle);
+  $('#contractButton').click(menuToggle);
+  $('#estimButton').click(menuToggle);
+  $('#contactButton').click(menuToggle);
+})
