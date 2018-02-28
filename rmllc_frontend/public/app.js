1fb99a8b3b04dff7d6f62e384b8d210c6572d740
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2018-02-28 15:45:18 -0500

    animated main menu

--- rmllc_frontend/public/app.js
+++ rmllc_frontend/public/app.js
@@ -6,25 +6,20 @@ $(()=>{
   const allSlides = [
     'url("./stylesheets/img/wwnurdem.bmp")',
     'url("./stylesheets/img/hvac.jpg")'
-  ]
+  ];
 
   // Toggling the menu bar buttons
-  const menuToggle = () => {
-    if (menuOpen == false) {
-      $('#allButtons').css('display','block');
-      menuOpen = true;
-    } else {
-      $('#allButtons').css('display','none');
-      menuOpen = false;
-    };
-  };
-  $('#topMenu').click(menuToggle);
-  $('#elecButton').click(menuToggle);
-  $('#hvacButton').click(menuToggle);
-  $('#refButton').click(menuToggle);
-  $('#contractButton').click(menuToggle);
-  $('#estimButton').click(menuToggle);
-  $('#contactButton').click(menuToggle);
+  // const menuToggle = () => {
+  //   if (menuOpen == false) {
+  //     $('#allButtons').css('display','block');
+  //     menuOpen = true;
+  //   } else {
+  //     $('#allButtons').css('display','none');
+  //     menuOpen = false;
+  //   };
+  //   console.log("menuToggle")
+  // };
+  // $('#topMenu').click(menuToggle);
 
   // Auto slideshow on title
   var slideNum = 0;
@@ -40,4 +35,25 @@ $(()=>{
   };
   changeSlide();
 
+  const dropMainMenu = () => {
+    if (menuOpen == false) {
+      $('#allButtons').css('display','block');
+      $('#allButtons').css('animation-play-state','running');
+      $('.oneButton').css('animation-play-state','running');
+      menuOpen = true;
+    } else if (menuOpen == true) {
+      $('#allButtons').css('display','none');
+      $('#allButtons').css('animation-play-state','paused');
+      $('.oneButton').css('animation-play-state','paused');
+      menuOpen = false;
+    }
+  }
+
+  $("#topMenu").click(dropMainMenu);
+  $('#elecButton').click(dropMainMenu);
+  $('#hvacButton').click(dropMainMenu);
+  $('#refButton').click(dropMainMenu);
+  $('#contractButton').click(dropMainMenu);
+  $('#estimButton').click(dropMainMenu);
+  $('#contactButton').click(dropMainMenu);
 })
